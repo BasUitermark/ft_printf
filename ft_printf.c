856d00a420a4ce74422bfea326d4e267b177edd2
Bas Uitermark <88148607+buiterma@users.noreply.github.com>
2021-11-17 20:43:48 +0100

    fixed iteration issue in while loop

--- ft_printf.c
+++ ft_printf.c
@@ -4,19 +4,18 @@ int	check_format(int out, va_list args, char *str, int i)
 {
 	if (str[i] == 'c' || str[i] == '%')
 		out += print_char(out, args);
-	// else if (str[i] == 's')
-	// 	out += print_str(out, args);
-	// else if (str[i] == 'p')
-	// 	out += print_void(out, args);
-	// else if (str[i] == 'd' || str[i] == 'i')
-	// 	out += print_digit(out, args);
-	// else if (str[i] == 'u')
-	// 	out += print_udigit(out, args);
-	// else if (str[i] == 'x')
-	// 	out += print_hex_lower(out, args);
-	// else if (str[i] == 'X')
-	// 	out += print_hex_upper(out, args);
-	// printf("check: %d\n", out);
+	else if (str[i] == 's')
+		out += print_str(out, args);
+	else if (str[i] == 'p')
+		out += print_void(out, args);
+	else if (str[i] == 'd' || str[i] == 'i')
+		out += print_digit(out, args);
+	else if (str[i] == 'u')
+		out += print_udigit(out, args);
+	else if (str[i] == 'x')
+		out += print_hex_lower(out, args);
+	else if (str[i] == 'X')
+		out += print_hex_upper(out, args);
 	return (out);
 }
 
@@ -35,8 +34,8 @@ int	ft_printf(const char *str, ...)
 	{
 		if (str[i] == '%')
 		{
-			i++;
 			out += check_format(out, args, (char *)str, i + 1);
+			i++;
 		}
 		else
 			out += write(1, &str[i], 1);
