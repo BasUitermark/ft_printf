74cbfa817c2e0aab112e209dc8e0a871be183dbf
Bas Uitermark <basuitermark@outlook.com>
2021-11-21 22:02:02 +0100

    unsigned fixed

--- main.c
+++ main.c
@@ -6,7 +6,12 @@ int	main(void)
 	int	b_write2;
 	char	*p = "asdfasdf";
 
-	b_write1 = printf("%p\n", -1);
-	b_write2 = ft_printf("%p\n", -1);
+	// b_write1 = printf("%c\n%s\n%i\n%p\n%x\n%X\n%u\n%%", 'a', "this is a string", 42, p, -1, -1, -1);
+	// b_write2 = ft_printf("%c\n%s\n%i\n%p\n%x\n%X\n%u\n%%", 'a', "this is a string", 42, p, -1, -1, -1);
+
+	b_write1 = printf("%X\n", -1);
+	b_write2 = ft_printf("%X\n", -1);
+
+	
 	printf("b_write1: %d\nb_write2: %d\n", b_write1, b_write2);
 }
--- src/print_udigit.c
+++ src/print_udigit.c
@@ -4,6 +4,6 @@ int	print_udigit(va_list args)
 {
 	int	out;
 
-	out = ft_putnbr_fd(va_arg(args, unsigned int), 1);
+	out = ft_putnbr_base_fd(va_arg(args, size_t), 1, "0123456789");
 	return (out);
 }
